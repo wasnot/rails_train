b07eab5339e04f13d68d305288fe7a80a067a4f2
wasnot <wasnot.apps@gmail.com>
2017-05-09 20:24:00 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'home/top'
+  get '/' => 'home#top'
+  get 'top' => 'home#top'
+  get 'about' => 'home#about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
